3e82e7c466252307be932ff156f787d8edae2261
NIK <nheger@gmail.com>
2025-04-23 11:22:26 +0800

    add scrips, made them executable

---
+++ generate_launcher_app_icons.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "--------------------------------------------------------"
+echo "Generating launcher app icons based on the config file flutter_launcher_icons.yaml"
+echo "To change launch or app icons on various platforms, modify the config file and run this script."
+echo "Documentation at https://pub.dev/packages/flutter_launcher_icons"
+echo "--------------------------------------------------------"
+echo "(c) 2025 Resonance Network"
+
+dart run flutter_launcher_icons
