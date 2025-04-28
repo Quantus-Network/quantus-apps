5736e793e066c9060799fa1ce1944a0bf14ffc12
NIK <nheger@gmail.com>
2025-04-28 15:43:40 +0800

    Update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'Quantus Wallet';
-  static const String tokenSymbol = 'QNT';
+  static const String tokenSymbol = 'QNTS';
 
   // static const String rpcEndpoint = 'ws://127.0.0.1:9944'; // local testing
   static const String rpcEndpoint = 'wss://a.t.res.fm:443';
