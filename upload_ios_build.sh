ff65cfe39d8711a51e654f344ae9deea0e288329
NIK <nheger@gmail.com>
2025-04-29 10:34:50 +0800

    Update upload_ios_build.sh

--- upload_ios_build.sh
+++ upload_ios_build.sh
@@ -2,5 +2,11 @@
 
 ## archive and upload the app to ios App store
 
+echo "Cleaning build folder"
+rm -rf build/ios/ipa/*.ipa
+
+echo "Building the app"
 flutter build ipa --release
-open -a "Transporter" "build/ios/ipa/Quantus Wallet.ipa"
+
+echo "Opening Transporter"
+open -a "Transporter" "build/ios/ipa/Quantus.ipa"
