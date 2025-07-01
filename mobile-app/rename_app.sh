3ec7f5c612e97e461bc5fc935b54e1f7b2dd90dd
Nikolaus Heger <nheger@gmail.com>
2025-07-01 13:00:38 +0800

    bundle id for ios change (#26)
    ios changed because the old id was already taken (by us I guess)

--- mobile-app/rename_app.sh
+++ mobile-app/rename_app.sh
@@ -5,5 +5,8 @@
 echo "Setting app name"
 rename setAppName --targets ios,android --value "Quantus Wallet"
 
-echo "Setting bundle id"
-rename setBundleId --targets ios,android --value "com.quantus.wallet"  
+echo "Setting bundle id ios"
+rename setBundleId --targets ios --value "com.quantus.mobile-wallet"  
+
+echo "Setting bundle id Android"
+rename setBundleId --targets android --value "com.quantus.wallet"
