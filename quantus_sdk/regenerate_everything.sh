9ca5971d808838c303e16ec4a43e78f609c8c9b0
NIK <nheger@gmail.com>
2025-07-23 08:48:34 +0700

    Cleaning up polkadart
    We need to delete old code before generating - generating code doesn't delete existing items so it just keeps adding and never deleted outdated or no longer existing metadata classes! Fixing this by adding a delete to the scripts. Also deleted all "polkadot" chain code, we don't need this.

--- quantus_sdk/regenerate_everything.sh
+++ quantus_sdk/regenerate_everything.sh
@@ -3,6 +3,9 @@
 echo "Generating Rust bindings..."
 flutter_rust_bridge_codegen generate
 
+echo "Deleting old Polkadart bindings"
+rm -rf lib/generated
+
 echo "Generating Polkadart bindings..."
 
 dart run polkadart_cli:generate -v
