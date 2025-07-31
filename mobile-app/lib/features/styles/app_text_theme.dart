cc2f62d812bc438a8e454acbd1d2bd2495d78163
NIK <nheger@gmail.com>
2025-07-31 21:46:23 +0800

    app extension, and iPad style (provisionary)

--- mobile-app/lib/features/styles/app_text_theme.dart
+++ mobile-app/lib/features/styles/app_text_theme.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+/// App text themes
+///
+/// Use with the extension!, like this:
+///             Text(
+///               'This is a small paragraph text.',
+///               style: context.smallParagraph, // Uses extension getter
+///             ),
+
+// AppTextTheme class (as provided previously)
 @immutable
 class AppTextTheme extends ThemeExtension<AppTextTheme> {
   final TextStyle? smallTitle;
@@ -23,42 +32,88 @@ class AppTextTheme extends ThemeExtension<AppTextTheme> {
         smallTitle: const TextStyle(
           fontSize: 18,
           fontWeight: FontWeight.w500,
-          fontFamily: "Fira Code",
+          fontFamily: 'Fira Code',
           fontStyle: FontStyle.normal,
           decoration: TextDecoration.none,
         ),
         paragraph: const TextStyle(
           fontSize: 16,
           fontWeight: FontWeight.w400,
-          fontFamily: "Fira Code",
+          fontFamily: 'Fira Code',
           fontStyle: FontStyle.normal,
           decoration: TextDecoration.none,
         ),
         smallParagraph: const TextStyle(
           fontSize: 14,
           fontWeight: FontWeight.w400,
-          fontFamily: "Fira Code",
+          fontFamily: 'Fira Code',
           fontStyle: FontStyle.normal,
           decoration: TextDecoration.none,
         ),
         largeTag: const TextStyle(
           fontSize: 16,
           fontWeight: FontWeight.w400,
-          fontFamily: "Fira Code",
+          fontFamily: 'Fira Code',
           fontStyle: FontStyle.normal,
           decoration: TextDecoration.none,
         ),
         detail: const TextStyle(
           fontSize: 12,
           fontWeight: FontWeight.w400,
-          fontFamily: "Fira Code",
+          fontFamily: 'Fira Code',
           fontStyle: FontStyle.normal,
           decoration: TextDecoration.none,
         ),
         tiny: const TextStyle(
           fontSize: 10,
           fontWeight: FontWeight.w400,
-          fontFamily: "Fira Code",
+          fontFamily: 'Fira Code',
+          fontStyle: FontStyle.normal,
+          decoration: TextDecoration.none,
+        ),
+      );
+
+  const AppTextTheme.iPad()
+    : this(
+        smallTitle: const TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w500,
+          fontFamily: 'Fira Code',
+          fontStyle: FontStyle.normal,
+          decoration: TextDecoration.none,
+        ),
+        paragraph: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'Fira Code',
+          fontStyle: FontStyle.normal,
+          decoration: TextDecoration.none,
+        ),
+        smallParagraph: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'Fira Code',
+          fontStyle: FontStyle.normal,
+          decoration: TextDecoration.none,
+        ),
+        largeTag: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'Fira Code',
+          fontStyle: FontStyle.normal,
+          decoration: TextDecoration.none,
+        ),
+        detail: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'Fira Code',
+          fontStyle: FontStyle.normal,
+          decoration: TextDecoration.none,
+        ),
+        tiny: const TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'Fira Code',
           fontStyle: FontStyle.normal,
           decoration: TextDecoration.none,
         ),
@@ -95,4 +150,17 @@ class AppTextTheme extends ThemeExtension<AppTextTheme> {
       tiny: TextStyle.lerp(tiny, other.tiny, t),
     );
   }
+}
+
+// Extension on BuildContext to access AppTextTheme styles directly
+extension AppTextThemeExtension on BuildContext {
+  TextStyle? get smallTitle =>
+      Theme.of(this).extension<AppTextTheme>()?.smallTitle;
+  TextStyle? get paragraph =>
+      Theme.of(this).extension<AppTextTheme>()?.paragraph;
+  TextStyle? get smallParagraph =>
+      Theme.of(this).extension<AppTextTheme>()?.smallParagraph;
+  TextStyle? get largeTag => Theme.of(this).extension<AppTextTheme>()?.largeTag;
+  TextStyle? get detail => Theme.of(this).extension<AppTextTheme>()?.detail;
+  TextStyle? get tiny => Theme.of(this).extension<AppTextTheme>()?.tiny;
 }
