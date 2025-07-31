8ca575b86dea0f8731d9fe1270633b10dd97cff3
NIK <nheger@gmail.com>
2025-07-31 12:38:28 +0800

    add deps, main provider

--- mobile-app/lib/main.dart
+++ mobile-app/lib/main.dart
@@ -1,26 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:resonance_network_wallet/features/main/screens/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quantus_sdk/quantus_sdk.dart';
-import 'package:resonance_network_wallet/models/wallet_state_manager.dart';
+import 'package:resonance_network_wallet/features/main/screens/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SubstrateService().initialize();
   await QuantusSdk.init();
 
-  final settingsService = SettingsService();
-  final chainHistoryService = ChainHistoryService();
-  final substrateService = SubstrateService();
-
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => WalletStateManager(
-        chainHistoryService,
-        settingsService,
-        substrateService,
-      ),
-      child: const ResonanceWalletApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: ResonanceWalletApp()));
 }
