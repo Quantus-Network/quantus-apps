11f0d21ff7dad78c36f1c7145dd685b7494e36f3
Nikolaus Heger <nheger@gmail.com>
2026-01-13 18:20:24 +0800

    code with less chance of overflow

--- quantus_sdk/lib/src/extensions/duration_extension.dart
+++ quantus_sdk/lib/src/extensions/duration_extension.dart
@@ -1,7 +1,7 @@
 import 'package:quantus_sdk/generated/schrodinger/types/qp_scheduler/block_number_or_timestamp.dart' as qp;
 
 extension DurationToTimestampExtension on Duration {
-  qp.Timestamp get qpTimestamp => qp.Timestamp(BigInt.from(inMilliseconds));
+  qp.Timestamp get qpTimestamp => qp.Timestamp(BigInt.from(inSeconds) * BigInt.from(1000));
 
-  static Duration fromQpTimestamp(qp.Timestamp timestamp) => Duration(milliseconds: timestamp.value0.toInt());
+  static Duration fromQpTimestamp(qp.Timestamp timestamp) => Duration(seconds: timestamp.value0.toInt() ~/ 1000);
 }
