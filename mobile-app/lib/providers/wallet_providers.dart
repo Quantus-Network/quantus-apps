3277449cd61ba9f7903838946f35270b4b78e024
NIK <nheger@gmail.com>
2026-01-14 20:26:37 +0800

    change watch to read

--- mobile-app/lib/providers/wallet_providers.dart
+++ mobile-app/lib/providers/wallet_providers.dart
@@ -119,7 +119,7 @@ BigInt _calculatePendingOutgoing(List<PendingTransactionEvent> pendingTransactio
 
 // fetch high security config
 final highSecurityConfigProvider = FutureProvider.family<HighSecurityData?, Account>((ref, account) async {
-  final service = ref.watch(highSecurityServiceProvider);
+  final service = ref.read(highSecurityServiceProvider);
   return service.getHighSecurityConfig(account.accountId);
 });
 
