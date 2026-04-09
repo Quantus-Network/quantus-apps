86da33b70c048f623ff95b9a4434f471eff60ac1
Nikolaus Heger <nheger@gmail.com>
2026-04-09 19:08:54 +0800

    use fake glass on android
    fixes severe rendering bugs

--- mobile-app/lib/v2/components/liquid_glass_base.dart
+++ mobile-app/lib/v2/components/liquid_glass_base.dart
@@ -1,3 +1,4 @@
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:liquid_glass_renderer/liquid_glass_renderer.dart';
 
@@ -30,31 +31,28 @@ class LiquidGlassBase extends StatelessWidget {
   }) : shape = Shape.circular,
        radius = 100.0;
 
+  LiquidGlassSettings get _settings => LiquidGlassSettings(
+    glassColor: glassColor,
+    visibility: visibility,
+    thickness: 20,
+    blur: 4,
+    refractiveIndex: 1.33,
+    lightAngle: 45 * (3.1416 / 180),
+    lightIntensity: 0.5,
+    ambientStrength: -0.2,
+    saturation: 1.5,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return LiquidGlassLayer(
-      settings: LiquidGlassSettings(
-        glassColor: glassColor,
-        visibility: visibility,
-        thickness: 20,
-        blur: 4,
-        refractiveIndex: 1.33,
-        lightAngle: 45 * (3.1416 / 180),
-        lightIntensity: 0.5,
-        ambientStrength: -0.2,
-        saturation: 1.5,
-      ),
-      child: centered
-          ? Center(
-              child: LiquidGlass(
-                shape: LiquidRoundedSuperellipse(borderRadius: radius),
-                child: child,
-              ),
-            )
-          : LiquidGlass(
-              shape: LiquidRoundedSuperellipse(borderRadius: radius),
-              child: child,
-            ),
-    );
+    final glassShape = LiquidRoundedSuperellipse(borderRadius: radius);
+    final Widget glass = Platform.isAndroid
+        ? FakeGlass(settings: _settings, shape: glassShape, child: child)
+        : LiquidGlassLayer(
+            settings: _settings,
+            child: LiquidGlass(shape: glassShape, child: child),
+          );
+
+    return centered ? Center(child: glass) : glass;
   }
 }
